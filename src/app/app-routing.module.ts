6be0d921cf8c96b05739e34dc6fe65f01c69e1a6
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2022-08-27 12:59:57 +0300

    make empty home page and registration form, add default navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {RegistrationPageComponent} from "./pages/registration-page/registration-page.component";
+import {NotFoundPageComponent} from "./pages/not-found-page/not-found-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'registration', component: RegistrationPageComponent},
+  {path: '**', component: NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    RegistrationPageComponent,
+    NotFoundPageComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
